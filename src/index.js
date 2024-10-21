476f8a65f07a0d775f72c4e09f274ce13f48e747
vinaykumarpandey <vinaypandey.mit12@gmail.com>
2024-10-21 16:21:16 +0530

    first my-app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <About />
   </React.StrictMode>
 );
 
